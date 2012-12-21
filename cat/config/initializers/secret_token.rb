a75f4793fb24b2e73f2bb12ce78e678776156751
freemanmp <megapilot83@mail.ru>
2012-12-21 22:50:58 +0200

    create

---
+++ cat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cat::Application.config.secret_token = '4e5274b12a93f6054ce76eb0fa137e02b6d2dccbd1c36b1b8cec9331e57d309ef78df72a023bad0e8fe5dc1dda3b3a32994fe143b2300bc6f919e1b71dda2287'
---
+++ shoptown/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoptown::Application.config.secret_token = '0b21697ddd9d9a6a5f634eee7d9a9e848aeb0c6b72a0489182e3c1f93867900f38f0f5d840ed318a95370e8e7132ae19d256624fa5bbb16ba3ec4b10a4c7208b'
